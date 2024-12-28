b9d9caa92b9d261edac629c16753a0e39de75b0a
M0ndares <omontesinos@ucol.mx>
2024-12-28 13:29:48 -0600

    Delete ts_config directory

--- ts_config/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: "ts-jest",
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-  coverageDirectory: "coverage",
-  collectCoverageFrom: ["src/**/*.{ts,tsx,js,jsx}", "!src/**/*.d.ts"],
-};
